b08dece480a274c4c65cfc9dc58d624493954c39
konomi_casa <konomi360@gmail.com>
2020-04-15 22:50:40 +0200

    ej5 y git ignore

--- bloque_1/ej_5.scm
+++ bloque_1/ej_5.scm
@@ -1,3 +1,30 @@
+#lang racket
 (define prob_total
   (lambda (x)
-    (if (null? list
+    (if (null? (list-tail x 1))
+        (cdr (list-ref x 0))
+        (+ (cdr (list-ref x 0)) (prob_total (list-tail x 1))))))
+
+
+(define encuentra_posicion
+  (lambda (lista threshold acumulado)
+    (set! acumulado (+ acumulado (cdr (list-ref lista 0))))
+    (if (> acumulado threshold)
+        (car (list-ref lista 0))
+        (encuentra_posicion (list-tail lista 1) threshold acumulado))))
+
+(define obtener-al-azar0
+  (lambda (x)
+    (encuentra_posicion x (* (random) (prob_total x)) 0)))
+
+(define elementos '())
+(do ((x 6000 (- x 1)))
+  ((= x 0) elementos)
+  (set! elementos
+        (cons (obtener-al-azar0 '((a . 1) (b . 2) (c . 3))) elementos)))
+
+(map (lambda (x)
+       (cons x (count (lambda(y) (eq? x y)) elementos)))
+     '(a b c))
+
+;(obtener-al-azar0 '((a . 1) (b . 2) (c . 3)))
