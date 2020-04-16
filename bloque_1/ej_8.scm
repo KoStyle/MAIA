7890560bda44be88954255ab8de249da087a0e4e
konomi_casa <konomi360@gmail.com>
2020-04-16 23:58:46 +0200

    Limpieza fichero ejemplos y ej8

--- bloque_1/ej_8.scm
+++ bloque_1/ej_8.scm
@@ -1,9 +1,12 @@
 #lang racket
 (define leer-ejemplos
   (lambda (x)
-    (if (not (string? x))
-        '()
-        (read (open-input-file x)))))
+    (call-with-input-file x
+     (lambda (i)
+       (let* ((a (read i)))
+         a)))))
+         
        
 
-(leer-ejemplos "C:\Users\konom\Desktop\IA_metodos_aprendizaje\bloque_1\ejemplos.scm")
+(define ejemplos (leer-ejemplos "C:\\Users\\konom\\Desktop\\IA_metodos_aprendizaje\\bloque_1\\ejemplos.scm"))
+(car (car ejemplos))
