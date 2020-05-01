1b0aa4241fe1dbc8a477ed592c206b2aad923586
konomi_casa <konomi360@gmail.com>
2020-05-02 00:24:44 +0200

    Correccion en 6, ej 12 (sin terminar)

--- bloque_1/ej_12.scm
+++ bloque_1/ej_12.scm
@@ -1,1 +1,34 @@
 #lang racket
+(require "ej_6.scm")
+(require "ej_9_inicio.scm")
+
+
+
+
+
+(define ejemplos (leer-ejemplos "C:\\Users\\konom\\Desktop\\IA_metodos_aprendizaje\\bloque_1\\ejemplos.scm"))
+
+(define eliminar_elemento
+  (lambda (lista item)
+    (if (null? lista)
+        '()
+        (if (eqv? (list-ref lista 0) item)
+            (list-tail lista 1)
+            (cons (list-ref lista 0) (eliminar_elemento (list-tail lista 1) item))))))
+
+
+
+;devuelve un dato al azar y lo elimina de la lista original
+;TODO hacer una funcion recursiva dentro de obtener_al_azar_etc y cambiar esta ultima a "separar"
+(define obtener_al_azar_sin_reemplazamiento
+  (lambda (perc x)
+    (if (null? x)
+        '()
+        (let ((elemento (obtener-al-azar x)))
+          (set! x (eliminar_elemento x elemento))
+          elemento))))
+
+ejemplos
+;(obtener-al-azar (car (list-tail ejemplos 1)))
+(obtener_al_azar_sin_reemplazamiento (car (list-tail ejemplos 1)))
+ejemplos
--- bloque_1/ej_6.scm
+++ bloque_1/ej_6.scm
@@ -1,4 +1,7 @@
 #lang racket
+(provide (all-defined-out))
+
+;obtiene la probabilidad total sumando todos los pesos de una lista de pares "(valor . peso)"
 (define prob_total
   (lambda (x)
     (if (null? (list-tail x 1))
@@ -6,6 +9,8 @@
         (+ (cdr (list-ref x 0)) (prob_total (list-tail x 1))))))
 
 
+
+;Busca el elemento que sobrepasa el threshold y devuelve el valor (sin el peso)
 (define encuentra_posicion
   (lambda (lista threshold acumulado)
     (set! acumulado (+ acumulado (cdr (list-ref lista 0))))
@@ -13,27 +18,29 @@
         (car (list-ref lista 0))
         (encuentra_posicion (list-tail lista 1) threshold acumulado))))
 
+;Convierte una lista de elementos simples en una lista de pares "(elemento . peso)" donde el peso es 1 para todos
 (define convertir-a-cons
   (lambda (x)
     (if (null? (list-tail x 1))
         (cons (cons (list-ref x 0) 1) '())
         (cons (cons (list-ref x 0) 1) (convertir-a-cons (list-tail x 1))))))
 
+;Obtiene un elemento al azar de una lista (uniforme)
 (define obtener-al-azar
   (lambda (x)
-    (when (not (pair? (list-ref x 0)))
+    (when (list? (cdr(list-ref x 0)))
       (set! x (convertir-a-cons x)))
     (encuentra_posicion x (* (random) (prob_total x)) 0)))
 
 ;(obtener-al-azar '((a . 1) (b . 2) (c . 3)))
 ;(obtener-al-azar '(a b c))
 
-(define elementos '())
-(do ((x 6000 (- x 1)))
-  ((= x 0) elementos)
-  (set! elementos
-        (cons (obtener-al-azar '(a b c)) elementos)))
-
-(map (lambda (x)
-       (cons x (count (lambda(y) (eq? x y)) elementos)))
-     '(a b c))
+;(define elementos '())
+;(do ((x 6000 (- x 1)))
+;  ((= x 0) elementos)
+;  (set! elementos
+;        (cons (obtener-al-azar '(a b c)) elementos)))
+;
+;(map (lambda (x)
+;       (cons x (count (lambda(y) (eq? x y)) elementos)))
+;     '(a b c))
