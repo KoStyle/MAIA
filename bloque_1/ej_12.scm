09a1277042ece1039066196f94fe1d4a0ccf2da5
konomi_casa <konomi360@gmail.com>
2020-05-19 19:28:02 +0200

    - Completado ej_12

--- bloque_1/ej_12.scm
+++ bloque_1/ej_12.scm
@@ -8,6 +8,12 @@
 
 (define ejemplos (leer-ejemplos "C:\\Users\\konom\\Desktop\\IA_metodos_aprendizaje\\bloque_1\\ejemplos.scm"))
 
+(define convertir_porcentaje
+  (lambda (x valor)
+    (if (or (not (real? x)) (not (integer? valor)) (< x 0))
+        '()
+        (ceiling (* x valor)))))
+
 (define eliminar_elemento
   (lambda (lista item)
     (if (null? lista)
@@ -20,15 +26,24 @@
 
 ;devuelve un dato al azar y lo elimina de la lista original
 ;TODO hacer una funcion recursiva dentro de obtener_al_azar_etc y cambiar esta ultima a "separar"
-(define obtener_al_azar_sin_reemplazamiento
-  (lambda (perc x)
-    (if (null? x)
+(define separar_rec
+  (lambda (tam nueva x)
+    (if (or (null? x) (not (integer? tam)) (not (list? nueva)))
         '()
-        (let ((elemento (obtener-al-azar x)))
-          (set! x (eliminar_elemento x elemento))
-          elemento))))
+        (if (< (length nueva) tam)
+            (let ((elemento (obtener-al-azar x)))
+              (set! x (eliminar_elemento x elemento))
+              (set! nueva (cons elemento nueva))
+              (separar_rec tam nueva x))
+            (list nueva x)))))
 
-ejemplos
-;(obtener-al-azar (car (list-tail ejemplos 1)))
-(obtener_al_azar_sin_reemplazamiento (car (list-tail ejemplos 1)))
-ejemplos
+(define separar 
+  (lambda (perc ejemp)
+    (if (or (not (real? perc)) (not (list? ejemp)))
+        '()
+        (let ((tam (convertir_porcentaje perc (length (list-tail ejemp 1)))))  ;Calculamos el tamaño de la lista a generar
+          (cons (list-ref ejemp 0) (separar_rec tam '() (list-tail ejemp 1))))))) ; devolvemos una lista con los  metadatos y dos listas de ejemplos, una con el porcentaje aproximado y otra con las sobras
+
+;ejemplos
+
+;(separar 0.33 ejemplos)
