f450742850a66ea65ad459440057e5c0db17628c
konomi_casa <konomi360@gmail.com>
2020-05-19 20:42:30 +0200

    - Completado ej_13 en fich ej_12

--- bloque_1/ej_12.scm
+++ bloque_1/ej_12.scm
@@ -44,6 +44,34 @@
         (let ((tam (convertir_porcentaje perc (length (list-tail ejemp 1)))))  ;Calculamos el tamaño de la lista a generar
           (cons (list-ref ejemp 0) (separar_rec tam '() (list-tail ejemp 1))))))) ; devolvemos una lista con los  metadatos y dos listas de ejemplos, una con el porcentaje aproximado y otra con las sobras
 
+;Esta funcion recursiva concatena resultados de la funcion split con el tamaño que le decimos (el resto a la hora de repartir los ejemplos de folds se reparte entre los n primeros folds) extrayendo de forma aleatoria
+;de la lista de ejemplos remains. fold_size el tamaño del fold, todo los folds que quedan por hacer, n_extra el numero de folds que quedan por tener un elemento extra.
+(define folds-rec
+  (lambda (remains todo n_extra fold_size)
+    (if (< todo 1)
+        '()
+        (if (= todo 1)
+            (cons remains '())
+            (if (> n_extra 0)
+                (let* ((split (separar_rec (+ 1 fold_size) '() remains))
+                       (nuevo_fold (car split)))
+                  (set! remains (list-ref split 1))
+                  (cons nuevo_fold (folds-rec remains (- todo 1) (- n_extra 1) fold_size)))
+                (let* ((split (separar_rec fold_size '() remains))
+                       (nuevo_fold (car split)))
+                  (set! remains (list-ref split 1))
+                  (cons nuevo_fold (folds-rec remains (- todo 1) n_extra fold_size))))))))
+
+(define folds
+  (lambda (num eje)
+    (if (or (not (integer? num)) (< num 1) (not (list? eje)))
+        '()
+        (let ((n_extra_lists (modulo (length (list-tail eje 1)) num))
+              (len_fold (floor (/ (length (list-tail eje 1)) num))))  ; Calculamos la longitud de los folds y cuantos de ellos tendran un elemento extra
+          (cons (list-ref eje 0) (folds-rec (list-tail eje 1) num n_extra_lists len_fold))))))
+
 ;ejemplos
 
 ;(separar 0.33 ejemplos)
+
+(folds 3 ejemplos)
